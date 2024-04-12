da3f4e8e513e3f682d1123edec8e92a0d9d392fe
Gleb Rodionov <137335455+ss3nclam@users.noreply.github.com>
2024-04-12 23:39:20 +0400

    upd

--- test_read_and_parse.py
+++ test_read_and_parse.py
@@ -1,13 +1,8 @@
 import logging
 
 import pandas
+from tb1_parser import Signal, TB1Parser
 
-from src.modules.tb1_parser.tb1_parser import TB1Parser
-from src.modules.tb1_parser.types._signal import Signal
-from src.modules.tb1_parser.types.signals_collection import SignalsCollection
-from src.modules.tb1_parser._tb1_file_reader import TB1FileReader
-from src.modules.tb1_parser.types.tb1_readed_sheets_collection import \
-    TB1ReadedSheetsCollection
 
 # Настройка логера
 logging.basicConfig(level=logging.DEBUG)
@@ -17,21 +12,12 @@ pandas.set_option("display.max_rows", None)
 pandas.set_option('display.max_colwidth', None)
 
 
+tb1 = TB1Parser('temp/table.xls')
+tb1.read()
 
-reader = TB1FileReader(filepath='temp/table.xls')
-
-reader.read()
-tb1: TB1ReadedSheetsCollection = reader.sheets
-for sheet_name, sheet_df in tb1.items():
-    print(sheet_df)
-
-parser = TB1Parser(tb1)
-parser.read()
-
-for name, collection in parser.collection.items():
-    collection: SignalsCollection
-    print(collection.signals_type)
-    for element in collection:
-        element: Signal
-        if element.name != 'Резерв':
-            print(element)
+for key, collection in tb1.collection.items():
+    print(key)
+    for signal in collection:
+        signal: Signal
+        if signal.name.lower() != 'резерв':
+            print(signal)
