fc36dda602dfb02d9ae63335ee55b9be97a9775b
Gleb Rodionov <137335455+ss3nclam@users.noreply.github.com>
2024-06-16 17:11:58 +0400

    upd

--- src/main.py
+++ src/main.py
@@ -1,13 +1,22 @@
+from os import listdir
 from tb1_parser import TB1Parser
 
 from .modules.report_maker import ReportMaker
 
 
 def main():
-    parser = TB1Parser("table.xlsx")
-    parser.read()
-    tb1 = parser.collection
+    excel_files = (
+        filename
+        for filename in listdir()
+        if filename.endswith((".xlsx", ".xls"))
+        and not filename.startswith("report")
+    )
 
-    report = ReportMaker(tb1)
-    report.make_sheets()
-    report.write("out.xlsx")
+    for filename in excel_files:
+        parser = TB1Parser(filename)
+        parser.read()
+        tb1 = parser.collection
+
+        report = ReportMaker(tb1)
+        report.make_sheets()
+        report.write(f"report_{filename.split('.')[0]}.xlsx")
